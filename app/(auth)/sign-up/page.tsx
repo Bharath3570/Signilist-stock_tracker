532adc5341256e9f5a5ea98ee7b820fff6128e35
Bharath <bharathn357@gmail.com>
2026-05-08 20:23:52 +0530

    Update

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -51,7 +51,7 @@ const SignUp = () => {
                 <InputField
                     name="fullName"
                     label="Full Name"
-                    placeholder="John Doe"
+                    placeholder="Bruce Wyane"
                     register={register}
                     error={errors.fullName}
                     validation={{ required: 'Full name is required', minLength: 2 }}
